d2533a154244ae0544afee99ec103dc03d4e3f65
carolina <carolinah@karunya.edu.in>
2026-04-11 00:59:30 +0530

    frontend-updations

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let total = 0;
 
+function addToCart(product, price) {
+  let table = document.getElementById("cartTable");
+
+  // Create new row
+  let row = table.insertRow();
+
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+
+  cell1.innerHTML = product;
+  cell2.innerHTML = "₹" + price;
+
+  // update total
+  total += price;
+  document.getElementById("total").innerText = "Total: ₹" + total;
+}
